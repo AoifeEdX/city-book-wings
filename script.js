b440baccd1fc00cb5cb1077466548146a525cc35
faithhopeandvanity <faithhopeandvanity@gmail.com>
2024-01-19 18:22:36 +0000

    revised queryurl

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function getbooks() {
 
   const queryURL = `http://openlibrary.org/subjects/london.json?q=london`;
   // /subjects/love.json
-
+// parse in city under 'subject' parameter
   fetch(queryURL)
     .then(function (response) {
       return response.json();
@@ -24,3 +24,6 @@ function bookSubject() {
 //a form input that allows the user to input a subject(city)
 //when button is clicked
 // onclick="getbooks()"
+
+//using for loop, create array to store first few book results
+//create 4 placeholder slots in cards for books in html file
