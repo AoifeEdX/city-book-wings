1070577475da4b72bb2bcc284b94dd2127d28754
Daniel <danielstoica078@gmail.com>
2024-01-24 20:50:07 +0000

    added about section, updated the nav bar with links and updated the internal nav link ids/ slideshow is now broken, trying to fix it ASAP.

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -1,12 +1,12 @@
 let slideIndex = 0;
 
 function showSlides() {
-    const imageSlides = document.getElementsByClassName("mySlides");
-    const textSlides = document.getElementsByClassName("mySlidesText");
+    const imageSlides = document.querySelectorAll(".mySlides");
+    const textSlides = document.querySelectorAll(".mySlidesText");
 
     for (let i = 0; i < imageSlides.length; i++) {
-        imageSlides[i].style.display = "none";
-        textSlides[i].classList.remove("show");
+        imageSlides[i].style.opacity = 0;
+        textSlides[i].style.opacity = 0;
     }
 
     slideIndex++;
@@ -14,12 +14,15 @@ function showSlides() {
         slideIndex = 1;
     }
 
-    imageSlides[slideIndex - 1].style.display = "block";
-    textSlides[slideIndex - 1].classList.add("show");
+    imageSlides[slideIndex - 1].style.opacity = 1;
+    textSlides[slideIndex - 1].style.opacity = 1;
 
     setTimeout(showSlides, 5000); // Change slide every 5 seconds (adjust as needed)
 }
 
 document.addEventListener("DOMContentLoaded", function () {
     showSlides();
+
+    // Immediately invoke the first iteration
+    setTimeout(showSlides, 0);
 });
