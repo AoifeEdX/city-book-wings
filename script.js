3a4c5f76d403ae1e5a58da87382d703dd1dfd8c4
Daniel <danielstoica078@gmail.com>
2024-01-24 22:13:29 +0000

    added JS books code

--- script.js
+++ script.js
@@ -1,47 +1,41 @@
-//function getbooks() {
-// const queryURL = `http://openlibrary.org/search.json?q=london`;
-
-function bookSubject() {
-var flightdestination = document.getElementById("citySearch").value;
-//const queryURL = `https://openlibrary.org/subjects?q=${flightdestination}&format=json`;
-const queryURL = `http://openlibrary.org/subjects/${flightdestination}.json?`;
-alert(flightdestination);
-fetch(queryURL)
-  .then(function (response) {
-  //  console.log(response)
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
-
+//create var for cities
+//make global so it can be used in other .js files
+//make sure each one matches the city name verbatim as configured in the API.
+var cities = [
+  "London, United Kingdom",
+  "Berlin, Germany",
+  "Dublin, Ireland",
+  "Madrid, Spain",
+  "Beijing, China",
+  "Mumbai, India",
+];
+function getbooks() {
+  //const queryURL = `http://openlibrary.org/search.json?q=london`
+  const queryURL = `http://openlibrary.org/subjects/london.json?q=london`;
+  // /subjects/love.json
+  fetch(queryURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
 }
+getbooks();
+function bookSubject() {
+  var flightdestination = document.getElementById("citysearch").value;
+  alert(flightdestination);
+}
+//target cities el in html
+var destCityName = document.querySelectorAll(".cities");
+console.log(destCityName);
+destCityName.forEach((list) => {
+  for (var i = 0; i < cities.length; i++) {
+    var opt = document.createElement("option");
+    opt.innerHTML = cities[i];
+    opt.value = cities[i];
+    list.appendChild(opt);
+  }
+});
+//loop through cities array
 bookSubject();
-
-
-
-//const queryURL = `http://openlibrary.org/subjects/search.json?q=${flightdestination}`;
-// /subjects/love.json
-
-
-//}
-//getbooks();
-
-//a form input that allows the user to input a subject(city)
-//when button is clicked
-// onclick="getbooks()"
-
-//add api key
-
-//const APIKey = "gu28zkekvzsey8np79k9kxm6";
-//const queryURL = `https://api.tugo.com/v1/travelsafe/country=&appid=${APIKey}`;
-
-//const queryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${APIKey}`;
-
-// fetch(queryURL)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     console.log(data);
-//   });
